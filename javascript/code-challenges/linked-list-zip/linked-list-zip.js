efc3ec3a253afb5a4f37e11839f7294fcb581576
Robert Mccreary <fooyinfighter@gmail.com>
2022-09-28 18:23:55 -0700

    can combine two sorted lists

--- javascript/code-challenges/linked-list-zip/linked-list-zip.js
+++ javascript/code-challenges/linked-list-zip/linked-list-zip.js
@@ -24,6 +24,9 @@ class LinkedList {
   zipWithList(list2) {
     return zipLists(this, list2);
   }
+  combineSortedLists(list2) {
+    return combineSortedLists(this, list2);
+  }
   toArray() {
     const arr = [];
     let node = this.head;
@@ -61,6 +64,40 @@ function zipLists(list1, list2) {
       node2 = node2.next;
     }
     if (node1) {
+      node.next = new Node(node1.value);
+      node = node.next;
+      node1 = node1.next;
+    }
+  }
+  return list;
+}
+
+function combineSortedLists(list1, list2) {
+  const list = new LinkedList();
+  let node;
+  let node1 = list1.head;
+  let node2 = list2.head;
+  if (node1) {
+    list.head = new Node(list1.head.value);
+    node = list.head;
+    node1 = node1.next;
+  } else if (node2) {
+    //list1 is empty
+    list.head = new Node(list2.head.value);
+    node = list.head;
+    node2 = node2.next;
+  } else {
+    // two empty lists
+    return list;
+  }
+  while (node1 || node2) {
+    // do node2 first, because we may have done node1 already.
+    if (node2 && (!node1 || node2.value < node1.value)) {
+      node.next = new Node(node2.value);
+      node = node.next;
+      node2 = node2.next;
+    }
+    if (node1 && (!node2 || node1.value < node2.value)) {
       node.next = new Node(node1.value);
       node = node.next;
       node1 = node1.next;
