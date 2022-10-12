395aeee2e826a774161d76737dca65866de60021
Robert Mccreary <fooyinfighter@gmail.com>
2022-10-12 15:58:11 -0700

    fix dirname

--- javascript/code-challenges/tree-fizz-buzz/read-words.js
+++ javascript/code-challenges/tree-fizz-buzz/read-words.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const readWordsFromFile = () => {
   const words = [];
-  const allFileContents = fs.readFileSync('data/word-list.txt', 'utf-8');
+  const allFileContents = fs.readFileSync(__dirname + '/data/word-list.txt', 'utf-8');
   allFileContents.split(/\r?\n/).forEach(line =>  {
     words.push(line)
   });
