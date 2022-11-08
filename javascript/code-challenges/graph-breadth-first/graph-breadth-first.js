f80c6fe35379df93067a80471cddec7941512d7c
Robert Mccreary <fooyinfighter@gmail.com>
2022-11-08 14:41:33 -0800

    tests and implementation

--- javascript/code-challenges/graph-breadth-first/graph-breadth-first.js
+++ javascript/code-challenges/graph-breadth-first/graph-breadth-first.js
@@ -2,7 +2,20 @@ const Graph = require('../graph/graph');
 
 class GraphWithTraversal extends Graph {
   breadthFirstTraversal(node, fn) {
-
+    const q = [node];
+    const visited = {};
+    while (q.length > 0) {
+      const node = q.pop();
+      fn(node.value);
+      visited[node.value] = true;
+      const neighbors = this.getNeighbors(node);
+      for (let n of neighbors) {
+        if (!(n.value in visited)) {
+          visited[n.value] = true;
+          q.unshift(n);
+        }
+      }
+    }
   }
 }
 
